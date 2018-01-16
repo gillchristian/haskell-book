454dae8ad0c73ae4c300fc07343d95fb0b4e54b9
gillchristian <gillchristiang@gmail.com>
2018-01-16 23:59:23 +0100

    Add exercises

---
+++ 15-monoid/Main.hs
@@ -0,0 +1,15 @@
+module MonoidT where
+
+import Data.Monoid
+
+data Optional a =
+    Nada
+  | Only a
+  deriving (Eq, Show)
+
+instance Monoid a => Monoid (Optional a) where
+  mempty = Nada
+
+  mappend (Only a) (Only b) = Only (mappend a b)
+  mappend Nada (Only b) = Only b
+  mappend (Only a) Nada = Only a

ba964cb2effde8ff16bcea599ffbbe063fbcaa79
gillchristian <gillchristiang@gmail.com>
2019-01-05 22:43:41 +0100

    WIP

--- 09-cipher.hs
+++ 09-cipher.hs
@@ -20,16 +20,24 @@ a = ord 'a'
 
 z = ord 'z'
 
+range = z - a + 1
+
 shift :: Offset -> Char -> Char
 shift o c
-  | m > z = chr $ (m `mod` z) + a - 1
+  | m > z = chr $ (m `mod` z) + a
   | otherwise = chr m
   where
-    m = ord c + o
+    m = ord c + o `mod` range
 
 unshift :: Offset -> Char -> Char
 unshift o c
-  | m < a = chr $ z - (a `mod` m) + 1
+  | m < a = chr $ z - (a `mod` m)
   | otherwise = chr m
   where
-    m = ord c - o
+    m = ord c - o `mod` range
+
+-- tests
+tests :: IO ()
+tests = do
+  print caesar 1 "a" == "b"
+  print caesar 25 "a" == "z"
